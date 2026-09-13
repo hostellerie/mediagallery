a2d1bf12d2cf3991ffb14128e05420a2843957a8
hostellerie <hostellerie.org@gmail.com>
2026-09-13 02:43:57 +0000

    Modernize remaining MediaGallery public templates

--- public_html/view.php
+++ public_html/view.php
@@ -112,10 +112,13 @@ $object = MG_buildContent($row, $opt);
 
 $T = COM_newTemplate(MG_getTemplatePath($aid));
 $T->set_file('video', 'view_window.thtml');
+$popupTitle = !empty($row['media_title']) ? strip_tags($row['media_title']) : $row['media_original_filename'];
+
 $T->set_var(array(
     'site_url' => $_MG_CONF['site_url'],
     'themeCSS' => $themeCSS,
     'charset'  => COM_getCharset(),
+    'title'    => MG_escapeHTML($popupTitle),
     'object'   => $object[0],
 ));
 
